a2e36279c516efc74c9bb49c65733a3ff5a6afea
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-24 21:10:27 +0800

    init: milkvac

---
+++ internal/clock/window.go
@@ -0,0 +1,30 @@
+package clock
+
+import (
+	"time"
+
+	"github.com/lacsar712/milkvac/internal/model"
+)
+
+type PulseWindow struct {
+	clk      Clock
+	duration time.Duration
+}
+
+func NewPulseWindow(clk Clock, duration time.Duration) *PulseWindow {
+	if duration <= 0 {
+		duration = 2 * time.Minute
+	}
+	return &PulseWindow{clk: clk, duration: duration}
+}
+
+func (w *PulseWindow) Active(anchor time.Time) bool {
+	return time.Since(anchor) < w.duration
+}
+
+func (w *PulseWindow) Require(anchor time.Time) error {
+	if w.Active(anchor) {
+		return nil
+	}
+	return model.ErrPulseHold
+}
