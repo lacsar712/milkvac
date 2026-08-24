a54974ab1079a0fc90e1334cd3d4127f8682eae7
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-24 21:09:24 +0800

    init: milkvac

---
+++ internal/app/alarms.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/lacsar712/milkvac/internal/interlock"
+	"github.com/lacsar712/milkvac/internal/model"
+)
+
+func (a *App) HandleVacTrip(ctx context.Context, tower model.TowerID, celsius float64) error {
+	if celsius <= a.cfg.TargetMoistPct+40 {
+		return nil
+	}
+	if err := a.guard.Permit(model.ZoneID(tower.String()+"-zone-00"), model.PlenumID("plenum-main")); err != nil {
+		return err
+	}
+	_ = interlock.DefaultLeaseTTL
+	return fmt.Errorf("heat alarm: %w", model.ErrVacTrip)
+}
